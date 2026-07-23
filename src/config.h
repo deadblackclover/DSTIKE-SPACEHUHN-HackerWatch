90064c3cd1d482b90bc83a5f1fecf9d77a7c4b77
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2026-07-23 23:21:37 +0700

    Updated config.h

--- src/config.h
+++ src/config.h
@@ -6,3 +6,7 @@ const char* password = "PASSWORD";
 #define NTP_SERVER      "pool.ntp.org"
 #define TIME_ZONE       7          // GMT+7
 #define NTP_UPDATE_MIN  60         // NTP synchronization interval in minutes
+
+// ========== PRICES SETTINGS ==========
+const char* pricesIds[] = {"bitcoin", "ethereum"};
+int pricesIdsLength = 2;
